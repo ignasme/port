180dac911a220c49be81767e8cc0c7254bd682b4
Whisper <hosse25uv@gmail.com>
2016-09-06 14:32:49 +0100

    update

---
+++ portfolio-master/assets/js/scroll.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.box.post').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).addClass('showme');
+            }
+            if( bottom_of_window < bottom_of_object ){
+                $(this).removeClass('showme');
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.box.highlight').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).addClass('showme');
+            }
+            if( bottom_of_window < bottom_of_object ){
+                $(this).removeClass('showme');
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.wrapper.style3').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).addClass('showme');
+            }
+            if( bottom_of_window < bottom_of_object ){
+                $(this).removeClass('showme');
+            }
+        });
+    });
+});
